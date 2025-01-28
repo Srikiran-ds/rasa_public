c0fb7afa555bdb50c78036d072e1cc22e85ad77b
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-01-29 02:20:49 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -33,7 +33,7 @@ df_costing = pd.read_excel('costing.xlsx')
 
 main_dashboard,costing=st.tabs(["Executive Dashboard","Costing"])
 
-costing.dataframe(df_costing)
+costing.dataframe(df_costing.sort_values('Costing’, ascending=False))
 
 df = df.iloc[1:]
 
