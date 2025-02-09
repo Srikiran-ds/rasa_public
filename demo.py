7f910b91e1391abc134b2c0d003d5938b4578b41
Srikiran Goud Chagapuram <64151178+Srikiran-ds@users.noreply.github.com>
2025-02-09 16:57:40 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -81,7 +81,7 @@ if True:
         else:
             df = pd.read_excel(uploaded_file_annexure, sheet_name='Order Level')
         return df
-    df = load_data(uploaded_file_annexure) 
+    df = load_data(for uploaded in uploaded_file_annexure) 
     @st.cache_data
     def load_data2(uploaded_file_orders):
         
